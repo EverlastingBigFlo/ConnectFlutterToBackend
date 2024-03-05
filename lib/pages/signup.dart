6a93854c5e0425677d29f5922b9259aa025f026f
everlastingbigflo <anjorin199@gmail.com>
2024-03-05 16:00:07 +0000

    5 Mar 2024, 17:00

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -21,8 +21,8 @@ class _SignUpState extends State<SignUp> {
         children: [
           Positioned(left: -80, top: 50, child: FirstCircle()),
           Positioned(left: 50, bottom: 60, child: SecondCircle()),
-          Positioned(top: 65, child: Create()),
-          Positioned(top: 75, child: Account()),
+          Positioned(top: 65, left: 30, child: Create()),
+          Positioned(top: 100, left: 30, child: Account()),
           Positioned(bottom: 60, left: 90, child: SignUpButton()),
         ],
       ),
