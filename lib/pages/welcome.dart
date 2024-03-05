aca1ad258a68bacbcfcfcb4b7f20b1636d585274
everlastingbigflo <anjorin199@gmail.com>
2024-03-05 12:54:08 +0000

    5 Mar 2024, 13:54

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -102,16 +102,14 @@ class _WelcomePageState extends State<WelcomePage> {
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
+              child: const Padding(
+                padding: EdgeInsets.all(20.0),
                 child: Text(
                   'JOIN NOW',
-                  style: GoogleFonts.inder(
-                    textStyle: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 36,
-                      fontWeight: FontWeight.w500,
-                    ),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
               ),
