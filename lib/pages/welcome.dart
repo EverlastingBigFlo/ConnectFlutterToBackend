9e811e888cc7f28bc41b64ba9289bb532028aab8
everlastingbigflo <anjorin199@gmail.com>
2024-03-05 12:36:48 +0000

    5 Mar 2024, 13:36

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomePage extends StatefulWidget {
-  const WelcomePage({super.key});
+  const WelcomePage({Key? key}) : super(key: key);
 
   @override
   State<WelcomePage> createState() => _WelcomePageState();
@@ -12,107 +13,112 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF0F4F3),
-      body: Stack(children: [
-        Positioned(
-          left: 20,
-          top: -150,
-          child: Container(
-            width: 460,
-            height: 460,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 193, 210, 220),
-              borderRadius: BorderRadius.circular(250),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  spreadRadius: 5,
-                  blurRadius: 20,
-                ),
-              ],
+      body: Stack(
+        children: [
+          Positioned(
+            left: 20,
+            top: -150,
+            child: Container(
+              width: 460,
+              height: 460,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 193, 210, 220),
+                borderRadius: BorderRadius.circular(250),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 5,
+                    blurRadius: 20,
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        Positioned(
-          left: 210,
-          bottom: 310,
-          child: Container(
-            width: 460,
-            height: 460,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 193, 210, 220),
-              borderRadius: BorderRadius.circular(250),
+          Positioned(
+            left: 210,
+            bottom: 310,
+            child: Container(
+              width: 460,
+              height: 460,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 193, 210, 220),
+                borderRadius: BorderRadius.circular(250),
+              ),
             ),
           ),
-        ),
-        const Positioned(
-          left: 270,
-          top: 140,
-          child: CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage('lib/assets/upimage.png'),
+          const Positioned(
+            left: 270,
+            top: 140,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('lib/assets/upimage.png'),
+            ),
           ),
-        ),
-        const Positioned(
+          const Positioned(
             left: 100,
             bottom: 400,
             child: CircleAvatar(
               radius: 90,
               backgroundImage: AssetImage('lib/assets/downimage.png'),
-            )),
-        const Positioned(
-          bottom: 150,
-          left: 60,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Let\'s Get',
-                style: TextStyle(
-                  fontSize: 75,
-                  fontWeight: FontWeight.bold,
-                  height: 1,
-                ),
-              ),
-              Text(
-                'Started',
-                style: TextStyle(
-                  fontSize: 75,
-                  fontWeight: FontWeight.bold,
-                  height: 1,
-                ),
-              ),
-              Text(
-                'Grow Together',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-        Positioned(
+          const Positioned(
+            bottom: 150,
+            left: 60,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Let\'s Get',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontWeight: FontWeight.bold,
+                    height: 1,
+                  ),
+                ),
+                Text(
+                  'Started',
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontWeight: FontWeight.bold,
+                    height: 1,
+                  ),
+                ),
+                Text(
+                  'Grow Together',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
             bottom: 70,
             left: 90,
             child: ElevatedButton(
-                onPressed: () => {},
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.black),
+              onPressed: () => {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'JOIN NOW',
+                  style: GoogleFonts.inder(
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ),
-                child: const Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                      'JOIN NOW',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w500,
-                        fontFamily:
-                            'Inder',
-                      ),
-                    ))))
-      ]),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
