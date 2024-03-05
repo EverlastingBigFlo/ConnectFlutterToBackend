b830fa4d60f98be323912e96a1cc16013fd2fcd4
everlastingbigflo <anjorin199@gmail.com>
2024-03-05 13:47:22 +0000

    5 Mar 2024, 14:47

--- lib/components/animations/let_get.dart
+++ lib/components/animations/let_get.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class LetGet extends StatefulWidget {
-  const LetGet({Key? key}) : super(key: key);
+  const LetGet({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _LetGetState createState() => _LetGetState();
 }
 
@@ -14,26 +15,16 @@ class _LetGetState extends State<LetGet> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 1),
-      vsync: this,
-    );
-    _animation = Tween<Offset>(
-      begin: const Offset(-1.0, 0.1),
-      end: Offset.zero,
-    ).animate(_controller);
-    // Start the animation after a delay
-    Future.delayed(const Duration(seconds: 3), () {
-      _controller.forward();
-    });
+    _controller =
+        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    _animation = Tween<Offset>(begin: const Offset(-1.0, 0.1), end: Offset.zero)
+        .animate(_controller);
+
+    _controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    // Check if _animation is null, if it is, return an empty SizedBox
-    if (_animation == null) {
-      return SizedBox.shrink();
-    }
     return SlideTransition(
       position: _animation,
       child: const Text(
