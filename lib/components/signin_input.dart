5c3f27340c7b118a87191405ba3180c8e18d059c
everlastingbigflo <anjorin199@gmail.com>
2024-03-06 14:31:33 +0000

    6 Mar 2024, 15:31

---
+++ lib/components/signin_input.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SignUpInput extends StatefulWidget {
+  const SignUpInput({super.key});
+
+  @override
+  State<SignUpInput> createState() => _SignUpInputState();
+}
+
+class _SignUpInputState extends State<SignUpInput> {
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            InputField(label: 'Enter Email Id'),
+            SizedBox(height: 40.0),
+            InputField(label: 'Create Username'),
+            SizedBox(height: 40.0),
+            InputField(label: 'Create Password', isObscure: true),
+            SizedBox(height: 40.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  final String label;
+  final bool isObscure;
+
+  const InputField({super.key, required this.label, this.isObscure = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: isObscure,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: const TextStyle(color: Colors.black, fontSize: 18),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.black, width: 2.0),
+        ),
+      ),
+    );
+  }
+}
