629b78cb2abdeb341f2132078c5331db1d182b29
everlastingbigflo <anjorin199@gmail.com>
2024-03-06 10:32:26 +0000

    6 Mar 2024, 11:32

--- lib/components/form_input.dart
+++ lib/components/form_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class FormInput extends StatefulWidget {
   const FormInput({super.key});
@@ -10,6 +11,43 @@ class FormInput extends StatefulWidget {
 class _FormInputState extends State<FormInput> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body:
+          Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InputField(label: 'Enter Email Id'),
+          SizedBox(height: 20.0),
+          InputField(label: 'Create Username'),
+          SizedBox(height: 20.0),
+          InputField(label: 'Create Password', isObscure: true),
+        ],
+      ),
+    )
+    );
+  }
+}
+
+
+class InputField extends StatelessWidget {
+  final String label;
+  final bool isObscure;
+
+  const InputField({super.key, required this.label, this.isObscure = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: isObscure,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: const TextStyle(color: Colors.grey),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.black),
+        ),
+      ),
+    );
   }
 }
