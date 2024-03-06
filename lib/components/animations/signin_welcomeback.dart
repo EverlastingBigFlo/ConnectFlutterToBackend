94798d19d205d9ff92f76d28b344124e0be06781
everlastingbigflo <anjorin199@gmail.com>
2024-03-06 14:35:23 +0000

    6 Mar 2024, 15:35

---
+++ lib/components/animations/signin_welcomeback.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class Create extends StatefulWidget {
+  const Create({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CreateState createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    _animation = Tween<Offset>(begin: const Offset(-2.0, 0.1), end: Offset.zero)
+        .animate(_controller);
+
+    Future.delayed(const Duration(seconds: 1), () {
+      _controller.forward();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _animation,
+      child: const Text(
+        'Create',
+        style: TextStyle(
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+          height: 1,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+// account widget
+
+class Account extends StatefulWidget {
+  const Account({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _AccountState createState() => _AccountState();
+}
+
+class _AccountState extends State<Account> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    _animation = Tween<Offset>(begin: const Offset(3.0, 0.1), end: Offset.zero)
+        .animate(_controller);
+
+    Future.delayed(const Duration(seconds: 1), () {
+      _controller.forward();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _animation,
+      child: const Text(
+        'Account :)',
+        style: TextStyle(
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+          height: 1,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
